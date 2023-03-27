42e3b009d078b9c65a0a27ceb87ffd87d518e5c3
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-03-27 12:05:52 +0300

    task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+
+const ingrItems = ingredients.map((ingr) => {
+
+  const ingrItem = document.createElement('li');
+
+  ingrItem.textContent = ingr;
+  ingrItem.classList.add('item');
+
+  return ingrItem;
+});
+
+listIngredients.append(...ingrItems);
