5ccd453f129bbcd53bffe12eb041d77157ac7f26
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-03-27 17:47:33 +0300

    task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,9 @@
+const nameInput = document.querySelector('#name-input');
+const nameOutput = document.querySelector('#name-output');
+
+nameInput.addEventListener('input', 
+	(evt) => {
+		if (evt.currentTarget.value) {
+			nameOutput.textContent = evt.currentTarget.value
+		} else nameOutput.textContent = 'Anonymous';
+	});
