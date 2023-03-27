cae6bf565545c7f9640863106cc0978c6bad19d8
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-03-27 21:37:04 +0300

    task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onSubmitCheckForm);
+
+function onSubmitCheckForm(evt) {
+	evt.preventDefault();
+	
+	const { email, password } = evt.currentTarget.elements;
+
+	if (!(email.value || password.value)) {
+		alert('All fields must be filled');
+		return; 
+	} else {
+		const formData = {
+		email: email.value,
+		password: password.value,
+		};	
+		
+		console.log(formData);
+
+		evt.currentTarget.reset();
+	};	
+};
