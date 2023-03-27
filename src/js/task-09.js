739d074746d4491edef21706d0dabc8f509b2c96
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-03-27 23:12:45 +0300

    task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const buttonEl = document.querySelector('.change-color');
+const spanEl = document.querySelector('.color');
+
+const onChangeColorBtn = buttonEl.addEventListener('click', onClick);
+
+function onClick(evt) {
+  spanEl.textContent = getRandomHexColor();
+  // console.log(spanEl.textContent);
+  bodyEl.style.backgroundColor = spanEl.textContent;
+}
